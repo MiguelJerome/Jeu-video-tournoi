ea9425addcd3d82ca7eedf9a47326d33c4dfe220
MiguelJerome <sharkmaycry@gmail.com>
2022-12-05 16:05:51 -0500

    add connexion errors message

--- public/js/connexion.js
+++ public/js/connexion.js
@@ -1,9 +1,43 @@
 let formAuth = document.getElementById('form-auth');
+
+// Nom usager: validation du formulaire connexion usager
 let inputCourriel = document.getElementById('input-courriel-utilisateur');
+let errorCourrielUsager = document.getElementById('error-courriel-authentification');
+
+const validateNomUsager = () => {
+    if(inputCourriel.validity.valid) {
+        errorCourrielUsager.style.display = 'none';
+    }
+    else if(inputCourriel.validity.valueMissing) {
+        errorCourrielUsager.innerText = 'Ce champ est requis';
+        errorCourrielUsager.style.display = 'block';
+    }
+};
+
+formAuth.addEventListener('submit', validateNomUsager);
+
 let inputMotDePasse = document.getElementById('input-mot-de-passe');
+let errorMotDePasseUsager = document.getElementById('error-MotDePasse-authentification');
+
+const validateMotDePasseUsager = () => {
+    if(inputMotDePasse.validity.valid) {
+        errorMotDePasselUsager.style.display = 'none';
+    }
+    else if(inputMotDePasse.validity.valueMissing) {
+        errorMotDePasseUsager.innerText = 'Ce champ est requis';
+        errorMotDePasseUsager.style.display = 'block';
+    }
+};
+
+formAuth.addEventListener('submit', validateMotDePasseUsager);
+
 
 formAuth.addEventListener('submit', async (event) => {
     event.preventDefault();
+
+    if(!formAuth.checkValidity()){
+        return;
+    }
 
     let data = {
         courriel: inputCourriel.value,
