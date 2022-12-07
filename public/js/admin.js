b2b76a0778aa98cc861b542031170492dc5236e5
MiguelJerome <sharkmaycry@gmail.com>
2022-12-07 07:25:09 -0500

    add the list of number participant per tournoi

--- public/js/admin.js
+++ public/js/admin.js
@@ -32,27 +32,33 @@ async function supprimerTournoi(id){
     });
 }
 
-
-
 async function getInscrits(){
     let response = await fetch('/nom-inscrits');
     
     if(response.ok){
         let data =await  response.json();
         
-        for( let i = 0;i<data.length;i++){
-            for(let  j =0;j<cardDivs.length;j++){
-                
+        let dernierTournament = 0;
+        let nombreParticapantParTournoi =1;
+        for(let i = 0;i<data.length;i++){
+            for(let  j =0; j<cardDivs.length;j++){
+                if(dernierTournament != data[i].id_tournois)
+                {
+                    dernierTournament = data[i].id_tournois;
+                    nombreParticapantParTournoi = 1;
+                }
+               
                 if(cardDivs[j].dataset.id == data[i].id_tournois){
                     
                     let p = document.createElement('p');
-                    p.innerText = `${data[i].prenom}  ${data[i].nom}`;
-                    cardDivs[j].append(p);      
+                    p.innerText = `${nombreParticapantParTournoi++} ${data[i].prenom}  ${data[i].nom}`;
+                    cardDivs[j].append(p); 
+                    i == data[i].id_tournois     
                 }
+                
             }
         }
-    }
-    
+    } 
 }
 
 getInscrits();
